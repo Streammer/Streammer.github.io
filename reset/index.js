f9e0149ebed747af41d1574b497e0e0ed215047f
dzmitry.lukashenka <Lukdmitrij@yandex.ru>
2021-09-21 10:43:10 +0300

    log ready message

--- reset/index.js
+++ reset/index.js
@@ -1,4 +1,9 @@
 var app = new window.Webex.Application()
+
+app.onReady()
+    .then(() => console.log('app is ready'))
+
+
 app.listen('application:displayContextChanged', (payload) => log('application:displayContextChanged', payload));
 app.listen('application:shareStateChanged', (payload) => log('application:shareStateChanged', payload));
 app.listen('application:themeChanged', (payload) => log('application:themeChanged', payload));
